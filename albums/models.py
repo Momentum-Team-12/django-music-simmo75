00ca73a7576587a45dfde76f9aaafb5bf796c76c
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-05-05 10:55:03 -0700

    got-django-music-on-page-sweeeeet

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
