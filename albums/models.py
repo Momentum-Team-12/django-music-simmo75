4e2c10dc37d10b7a2ebbf6c38697c76eb4f64d60
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-05-05 11:48:39 -0700

    database-album-artist-added

--- albums/models.py
+++ albums/models.py
@@ -7,3 +7,6 @@ class Album(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
